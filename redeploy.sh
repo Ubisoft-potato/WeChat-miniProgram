fd2f8866860dc05c63437aa83174b7e640b66ab9
ubisoft-potato <1354250064mdzz@gmail.com>
2019-11-03 20:24:20 +0800

    修改部署脚本

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,6 @@
 kubectl delete -f  ./k8s-deployment/photoshare-service/deployment.yaml
 mvn clean package
-docker rmi  photoshare:dev
+docker rmi -f photoshare:dev
 docker build -t photoshare:dev .
 kubectl apply -f  ./k8s-deployment/photoshare-service/deployment.yaml
+
