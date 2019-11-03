0ea7d1d18e8caf8379564604cfa435602b9ea786
ubisoft-potato <1354250064mdzz@gmail.com>
2019-11-03 20:48:56 +0800

    修改部署脚本

--- docker-image-clean.sh
+++ docker-image-clean.sh
@@ -1,2 +1,4 @@
 echo "----------------start remove old images----------------"
 docker rmi -f $(docker images -f "dangling=true" -q)
+echo "/n/n"
+docker images
--- redeploy.sh
+++ redeploy.sh
@@ -3,3 +3,6 @@ mvn clean package
 docker rmi -f photoshare:dev
 docker build -t photoshare:dev .
 kubectl apply -f  ./k8s-deployment/photoshare-service/deployment.yaml
+echo "/n/n"
+
+kubectl get pod -A
